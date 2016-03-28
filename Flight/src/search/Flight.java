2da9d7ff914e44d3ffe1d9f9e8e514a8c9bd3b7d
solvimarm <solvimar@gmail.com>
2016-03-28 16:38:13 +0000

    testers

--- Flight/src/search/Flight.java
+++ Flight/src/search/Flight.java
@@ -1,5 +1,46 @@
 package search;
+import java.util.Date;
 
+//import search.FlightSearch;
 public class Flight {
-
+	
+	private String flightNO;
+	private Date DepartureTime;
+	private Date ArrivalTime;
+	private int Price;
+	private String to;
+	private String from;
+	private int seatsAvailable;
+	
+	public Flight(String flightNO, Date DepartureTime, Date ArrivalTime, int Price, String to, String from, int seatsAvailable){
+		this.flightNO = flightNO;
+		this.DepartureTime = DepartureTime;
+		this.ArrivalTime = ArrivalTime;
+		this.Price = Price;
+		this.to = to;
+		this.from = from;
+		this.seatsAvailable = seatsAvailable;
+	}
+	
+	public Date getDepartureTime(){
+		return DepartureTime;
+	}
+	public Date getArrivalTime(){
+		return ArrivalTime;
+	}
+	public String getflightNO(){
+		return flightNO;
+	}
+	public int getPrice(){
+		return Price;
+	}
+	public String getto(){
+		return to;
+	}
+	public String getfrom(){
+		return from;
+	}
+	public int getSeatsAvailable(){
+		return seatsAvailable;
+	}
 }
--- Flight/src/search/FlightSearch.java
+++ Flight/src/search/FlightSearch.java
@@ -1,12 +1,19 @@
 package search;
 
+import java.util.Date;
+import java.util.List;
+//import java.util.ArrayList;
+
 import search.DB_connection;
 
 public class FlightSearch {
 	static DB_connection db = new DB_connection();
-	public static String searchReturn(){
-		String s = db.findDep();
-		return s;
+	
+	public static List<Flight> searchDeparture(Date Dtime, int ppltrav, String To, String From){
+		return null;
+	}
+	public static List<Flight> searchReturn(Date Rtime, int ppltrav, String To, String From){
+		return null;
 	}
 
 }
