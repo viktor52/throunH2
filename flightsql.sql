bd8597808d7ed2e8c662223a29594904e25a789c
vitkor52 <viktorymir91@gmail.com>
2016-04-12 14:59:08 +0000

    mm

---
+++ flightsql.sql
@@ -0,0 +1,24 @@
+﻿INSERT INTO flight(
+            "number", arivalairport, id, depdate, departureairport, price)
+    VALUES ('FK120', 'KEF', 1, date '2016-09-09','AEY' , '1000ISK');
+INSERT INTO flight(
+            "number", arivalairport, id, depdate, departureairport, price)
+    VALUES ('FK121', 'AEY', 2, date '2016-09-18','KEF' , '1000ISK');
+INSERT INTO flight(
+            "number", arivalairport, id, depdate, departureairport, price)
+    VALUES ('FK122', 'RVK', 3, date '2016-09-09','EGS' , '1000ISK');
+INSERT INTO flight(
+            "number", arivalairport, id, depdate, departureairport, price)
+    VALUES ('FK123', 'EGS', 4, date '2016-09-18','RVK' , '1000ISK');
+INSERT INTO flight(
+            "number", arivalairport, id, depdate, departureairport, price)
+    VALUES ('FK124', 'IFJ', 5, date '2016-09-09','GRY' , '1000ISK');
+INSERT INTO flight(
+            "number", arivalairport, id, depdate, departureairport, price)
+    VALUES ('FK125', 'GRY', 6, date '2016-09-18','IFJ' , '1000ISK');
+INSERT INTO flight(
+            "number", arivalairport, id, depdate, departureairport, price)
+    VALUES ('FK126', 'THO', 7, date '2016-09-09','VPN' , '1000ISK');
+INSERT INTO flight(
+            "number", arivalairport, id, depdate, departureairport, price)
+    VALUES ('FK127', 'VPN', 8, date '2016-09-18','THO' , '1000ISK');
