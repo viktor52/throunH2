e475c1ee1b073d65d40c373f3b06f14b534000ee
vitkor52 <viktorymir91@gmail.com>
2016-04-14 18:56:54 +0000

    mm

--- Flight/src/search/selectFlight.java
+++ Flight/src/search/selectFlight.java
@@ -46,7 +46,7 @@ public class selectFlight extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public selectFlight(Vector<Vector<Object>> data2,Vector<Vector<Object>> retdata2, Vector<String> headers2) {
+	public selectFlight(Vector<Vector<Object>> data2,Vector<Vector<Object>> retdata2, Vector<String> headers2, final boolean yes) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 550);
 		contentPane = new JPanel();
@@ -80,6 +80,7 @@ public class selectFlight extends JFrame {
 		JLabel lblNewLabel_1 = new JLabel("Returning Flights");
 
 		lblNewLabel_1.setBounds(224, 242, 113, 15);
+		lblNewLabel_1.setVisible(yes);
 		contentPane.add(lblNewLabel_1);
         
 		final JTable table2 = new JTable( retdata2, headers2 ){
@@ -88,10 +89,12 @@ public class selectFlight extends JFrame {
 				  }
 		};
         //table2.setBounds(1, 1, 450, 0);
+		table2.setVisible(yes);
         contentPane.add(table2);
 
         JScrollPane scrollPane_1 = new JScrollPane(table2);
 		scrollPane_1.setBounds(40, 280, 517, 184);
+		scrollPane_1.setVisible(yes);
 		contentPane.add(scrollPane_1);
 		//////////////////////////////////////////////////////
 		JButton nextStep = new JButton("Next");
@@ -106,14 +109,15 @@ public class selectFlight extends JFrame {
 					d[i] = table.getValueAt(row, i);
 				}
 				System.out.println((String)d[0]);
-				
-				int rerow =table2.getSelectedRow();
-				int recolumn = table2.getColumnCount();
-				Object [] re = new Object[recolumn];
-				for(int i = 0; i<recolumn;i++){
-					re[i] = table2.getValueAt(row, i);
+				if(yes){
+					int rerow =table2.getSelectedRow();
+					int recolumn = table2.getColumnCount();
+					Object [] re = new Object[recolumn];
+					for(int i = 0; i<recolumn;i++){
+						re[i] = table2.getValueAt(row, i);
+					}
+					System.out.println((String)re[0]);
 				}
-				System.out.println((String)re[0]);
 			}
 		});
 		nextStep.setBounds(248, 476, 66, 25);
@@ -123,12 +127,12 @@ public class selectFlight extends JFrame {
 	public boolean isCellEditable(int row, int column){
 		return false;
 	}
-	public static void getInfo(Date f,Date t, int a, int b, int c, String til, String fra, boolean yes){
+	public static void getInfo(Date f,Date t, int a, int b, int c, String til, String fra, final boolean yes){
 		EventQueue.invokeLater(new Runnable() {
 			
 			public void run() {
 				try {
-					selectFlight frame1 = new selectFlight(data,retdata, headers);
+					selectFlight frame1 = new selectFlight(data,retdata, headers, yes );
 					frame1.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -149,19 +153,22 @@ public class selectFlight extends JFrame {
 		        row.add(k.getPrice());
 		        data.add(row);
 			}
-			List<Flight> ret = fs.searchReturn(t, a,b,c, fra, til);
-			Iterator<Flight> itre = ret.iterator();
 			
-			while(itre.hasNext()){
-				Flight k  = itre.next();
-				System.out.println(k.getflightNO()+", fra:"+k.getfrom()+", til:"+k.getTo()+","+k.getPrice());
-			    Vector<Object> row = new Vector<Object>();
-			    row.add( k.getflightNO());
-			    row.add( k.getfrom());
-			    row.add( k.getTo());
-		        row.add(k.getDepartureTime());
-		        row.add(k.getPrice());
-		        retdata.add(row);
+			if(yes){
+				List<Flight> ret = fs.searchReturn(t, a,b,c, fra, til);
+				Iterator<Flight> itre = ret.iterator();
+				
+				while(itre.hasNext()){
+					Flight k  = itre.next();
+					System.out.println(k.getflightNO()+", fra:"+k.getfrom()+", til:"+k.getTo()+","+k.getPrice());
+				    Vector<Object> row = new Vector<Object>();
+				    row.add( k.getflightNO());
+				    row.add( k.getfrom());
+				    row.add( k.getTo());
+			        row.add(k.getDepartureTime());
+			        row.add(k.getPrice());
+			        retdata.add(row);
+				}
 			}
 		
         
