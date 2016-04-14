7487647609628c26102129ade2d872c65bd6d654
astroseinarsdottir <astros.einars@gmail.com>
2016-04-14 14:54:56 +0000

    laga person

--- Flight/src/search/Person.java
+++ Flight/src/search/Person.java
@@ -7,13 +7,17 @@ public class Person {
 	private String birthday;
 	private String email;
 	private int phoneNumber;
+	private String seat;
+	private int orderNumber;
 	
-	public Person( String name, String birthday, String email, int phoneNumber ){
+	public Person( String name, String birthday, String email, int phoneNumber, String seat, int orderNumber ){
 		
 		this.name = name;
 		this.birthday = birthday;
 		this.email = email;
 		this.phoneNumber = phoneNumber;
+		this.seat = seat;
+		this.orderNumber = orderNumber;
 	}
 	
 	public String getName(){
@@ -31,5 +35,13 @@ public class Person {
 	public int getPhoneNumber(){
 		
 		return phoneNumber;
+	}
+	public String getSeat(){
+		
+		return seat;
+	}
+	public int getOrderNumber(){
+		
+		return orderNumber;
 	}
 }
